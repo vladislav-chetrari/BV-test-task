22aad3a0e4a7362ccbb879b6ee1ddf29d4d4f2cd
vchetrari <i79182602@gmail.com>
2021-10-13 21:15:04 +0300

    MainActivity: moved to ui package, extends BaseActivity

---
+++ app/src/main/java/vlad/chetrari/bvtesttask/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package vlad.chetrari.bvtesttask.ui
+
+import android.content.res.Resources
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import vlad.chetrari.bvtesttask.R
+import vlad.chetrari.bvtesttask.base.BaseActivity
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity(R.layout.activity_main) {
+
+    @Inject
+    //checking di works
+    lateinit var res: Resources
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //checking di works
+        Timber.d("resources = $res")
+    }
+}
