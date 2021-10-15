76c89ea976d3e5981bacb5948504a53034175ab3
vchetrari <i79182602@gmail.com>
2021-10-15 22:14:22 +0300

    TwitStreamSearchFragment set up

---
+++ app/src/main/java/vlad/chetrari/bvtesttask/app/main/stream/search/v1/TwitListAdapter.kt
@@ -0,0 +1,47 @@
+package vlad.chetrari.bvtesttask.app.main.stream.search.v1
+
+import android.content.res.Resources
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import vlad.chetrari.bvtesttask.R
+import vlad.chetrari.bvtesttask.data.model.ui.SearchStreamTwit
+
+class TwitListAdapter : ListAdapter<SearchStreamTwit, TwitListAdapter.ViewHolder>(ItemCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.list_item_twit_v1, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(getItem(position))
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val res: Resources
+            get() = itemView.resources
+        private val userImage: ImageView
+            get() = itemView.findViewById(R.id.userImage)
+        private val userScreenName: TextView
+            get() = itemView.findViewById(R.id.userScreenName)
+        private val userDescription: TextView
+            get() = itemView.findViewById(R.id.userDescription)
+        private val twitText: TextView
+            get() = itemView.findViewById(R.id.twitText)
+
+        fun bind(twit: SearchStreamTwit) {
+            userScreenName.text = res.getString(R.string.twit_screen_name_format, twit.userScreenName)
+            userDescription.text = twit.userDescription
+            twitText.text = twit.text
+        }
+    }
+
+    private class ItemCallback : DiffUtil.ItemCallback<SearchStreamTwit>() {
+        override fun areItemsTheSame(oldItem: SearchStreamTwit, newItem: SearchStreamTwit) = oldItem.id == newItem.id
+        override fun areContentsTheSame(oldItem: SearchStreamTwit, newItem: SearchStreamTwit) = oldItem == newItem
+    }
+}
---
+++ app/src/main/java/vlad/chetrari/bvtesttask/app/main/stream/search/v1/TwitStreamSearchViewModel.kt
@@ -0,0 +1,61 @@
+package vlad.chetrari.bvtesttask.app.main.stream.search.v1
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.switchMap
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.disposables.Disposable
+import timber.log.Timber
+import vlad.chetrari.bvtesttask.app.base.BaseViewModel
+import vlad.chetrari.bvtesttask.data.model.ui.SearchStreamTwit
+import vlad.chetrari.bvtesttask.data.network.client.TwitterSearchClient
+import java.net.SocketTimeoutException
+import java.util.*
+import javax.inject.Inject
+
+@HiltViewModel
+class TwitStreamSearchViewModel @Inject constructor(
+    private val client: TwitterSearchClient
+) : BaseViewModel() {
+
+    private var twitStreamDisposable: Disposable? = null
+
+    private val searchKeyword = MutableLiveData<String>()
+    val twits: LiveData<List<SearchStreamTwit>> = searchKeyword.switchMap { twitStreamLiveData(it) }
+
+    fun onSearchQuery(query: String) = searchKeyword.postValue(query)
+
+    private fun twitStreamLiveData(query: String): LiveData<List<SearchStreamTwit>> {
+        val liveData = MutableLiveData<List<SearchStreamTwit>>()
+        twitStreamDisposable?.dispose()
+        if (query.isNotBlank()) {
+            val twitList = LinkedList<SearchStreamTwit>()
+            twitStreamDisposable = client.search(query)
+                .doOnNext { Timber.d("row fetched = $it") }
+                .subscribe({ str ->
+                    twitList.add(str)
+                    liveData.postValue(twitList.sortedByDescending { it.timestampEpochMillis })
+                }, ::onError)
+        } else {
+            liveData.postValue(emptyList())
+        }
+        return liveData
+    }
+
+    private fun restartTwitStream() = searchKeyword.postValue(searchKeyword.value)
+
+    override fun onError(error: Throwable) {
+        when (error) {
+            is SocketTimeoutException -> {
+                Timber.w("SocketTimeoutException capt, restarting the stream")
+                restartTwitStream()
+            }
+            else -> super.onError(error)
+        }
+    }
+
+    override fun onCleared() {
+        twitStreamDisposable?.dispose()
+        super.onCleared()
+    }
+}
